771ebe98a34993867d6cbbc26264cf1336ce94c7
Nelson <nbritob@hotmail.com>
2019-11-08 17:30:23 +0000

    Table order re-arragned, forgot the int on country and city ids, tables running fine now

--- db/bucket_list.sql
+++ db/bucket_list.sql
@@ -2,23 +2,20 @@ DROP TABLE visits;
 DROP TABLE cities;
 DROP TABLE countries;
 
-CREATE TABLE visits
-(
-  id SERIAL8 PRIMARY KEY,
-  country_id REFERENCES countries(id) ON DELETE CASCADE,
-  city_id REFERENCES cities(id) ON DELETE CASCADE,
-  visited VARCHAR(255)
+CREATE TABLE countries(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
 );
 
-CREATE TABLE cities
-(
-  id SERIAL8 PRIMARY KEY,
+CREATE TABLE cities(
+  id SERIAL4 PRIMARY KEY,
   name VARCHAR(255),
   country_id INT4 REFERENCES countries(id) ON DELETE CASCADE
 );
 
-CREATE TABLE countries
-(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255)
+CREATE TABLE visits(
+  id SERIAL4 PRIMARY KEY,
+  country_id INT4 REFERENCES countries(id) ON DELETE CASCADE,
+  city_id INT4 REFERENCES cities(id) ON DELETE CASCADE,
+  visited VARCHAR(255)
 );
