22df419a458425537c008ea12f80286f728e0675
Nelson <nbritob@hotmail.com>
2019-11-09 14:23:15 +0000

    save for city added may re-arrange order in the future just so stuff matches

--- models/city.rb
+++ models/city.rb
@@ -11,7 +11,15 @@ class City
     @name = options['name']
     @country_id = options['country_id'].to_i
 
-    
+  def save()
+    sql = "INSERT INTO cities (name, country_id)
+    VALUES($1, $2)
+    RETURNING id;"   #does order matter in save? I think so?
+    values = [@name, @country_id]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
+  end
+
 
 
 end
