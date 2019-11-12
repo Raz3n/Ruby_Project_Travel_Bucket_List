85398c882b45a07216cc3e1ee964faa9fd92354e
Nelson <nbritob@hotmail.com>
2019-11-12 16:58:00 +0000

    MVP Done

--- app.rb
+++ app.rb
@@ -1,12 +1,11 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('./controllers/cities_controller.rb')
-require_relative('./controllers/countries_controller.rb')
+require_relative('controllers/cities_controller.rb')
+require_relative('controllers/countries_controller.rb')
 also_reload('./models/*')
-
 
 get '/' do
   @countries = Country.all
   @cities = City.all
-  erb( :index )
+  erb(:index)
 end
