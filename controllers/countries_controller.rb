15f2b6907de7d15e91e908fac7c789e506ba15ee
Nelson <nbritob@hotmail.com>
2019-11-12 15:24:35 +0000

    working on new for countries

--- controllers/countries_controller.rb
+++ controllers/countries_controller.rb
@@ -6,10 +6,15 @@ also_reload( '../models/*' )
 
 get '/countries' do
   @countries = Country.all()
-  erb(:"countries/index")
+  erb(:'countries/index')
 end
 
-get '/countries/:id' do
-  @countries = Country.find(params['id'].to_i)
-  erb( :"country/show" )
+get "/countries/:id" do
+  @countries = Country.find(params[:id])
+  erb(:'country/show')
+end
+
+get "/countries/new" do
+  @countries = Country.all()
+  erb(:'countries/new')
 end
